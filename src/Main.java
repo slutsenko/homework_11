498448560fe60241c9273be6f900c7ae628e364e
slutsenko <lucik3003@gmail.com>
2021-03-21 18:59:18 +0200

    homework11

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+import java.util.Random;
+
+public class Main {
+    static Person[] allPersons = new Person[100];
+
+
+    public static void main(String[] args) {
+
+        for (int j = 0; j < allPersons.length; j++) {
+            String names[] = {"Max", "John", "Mike", "Nick", "Jack", "Paul"};
+            Random random = new Random();
+            int index = random.nextInt(names.length);
+            allPersons[j] = new Person(names[index], "Ivanov");
+        }
+
+        for (int i = 0; i < allPersons.length; i++) {
+            System.out.print(allPersons[i].firstName + ", ");
+
+        }
+    }
+}
+
+
+
---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+    int height;
+    int weight;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+}
+
+
